e2b08a1e4f30addf38aaa59454d92ba29f0a785e
Mike Solomon <mike@mindsnacks.com>
2013-05-06 23:19:31 -0700

    Updated organization model

--- app/models/organization.go
+++ app/models/organization.go
@@ -1,7 +1,8 @@
 package models
 
 type Organization struct {
-	Id   int    `json:"id"`
-	Name string `json:"name"`
-	Slug string `json:"slug"`
+	Id               int    `json:"id"`
+	Name             string `json:"name"`
+	Slug             string `json:"slug"`
+	Default_timezone string `json:"default_timezone"`
 }
