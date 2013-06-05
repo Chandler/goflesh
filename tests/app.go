e5a149fd800ec00dd9b9c8a31df639fd38277a22
Mike Solomon <mike@mindsnacks.com>
2013-06-04 20:34:10 -0700

    Updated test to use pointers

--- tests/app.go
+++ tests/app.go
@@ -8,16 +8,16 @@ type ApplicationTest struct {
 	revel.TestSuite
 }
 
-func (t ApplicationTest) Before() {
+func (t *ApplicationTest) Before() {
 	TestInit()
 }
 
-func (t ApplicationTest) TestIndexPageWorks() {
+func (t *ApplicationTest) TestIndexPageWorks() {
 	t.Get("/")
 	t.AssertOk()
 	t.AssertContentType("text/html")
 }
 
-func (t ApplicationTest) After() {
+func (t *ApplicationTest) After() {
 	TestClean()
 }
