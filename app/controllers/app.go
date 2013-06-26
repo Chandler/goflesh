114df9652bd5bfa98e1530207f2e4c9d2f0ec530
Mike Solomon <mike@mindsnacks.com>
2013-06-25 21:33:50 -0700

    Removed init.go

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,9 +1,18 @@
 package controllers
 
-import "github.com/robfig/revel"
+import (
+	"github.com/robfig/revel"
+)
 
 type Application struct {
 	*revel.Controller
+}
+
+func init() {
+	revel.OnAppStart(Init)
+	revel.InterceptMethod((*GorpController).Begin, revel.BEFORE)
+	revel.InterceptMethod((*GorpController).Commit, revel.AFTER)
+	revel.InterceptMethod((*GorpController).Rollback, revel.FINALLY)
 }
 
 func (c Application) Index() revel.Result {
