1e01816e71accf0874996b612e25ae492906aa87
Mike Solomon <mike@mindsnacks.com>
2013-07-05 18:46:33 -0700

    Added OZ Pool model

---
+++ app/controllers/oz_pool.go
@@ -0,0 +1,100 @@
+package controllers
+
+import (
+	"encoding/json"
+	"errors"
+	"flesh/app/models"
+	"github.com/robfig/revel"
+)
+
+type OzPools struct {
+	GorpController
+}
+
+/////////////////////////////////////////////////////////////////////
+
+func (c OzPools) ReadList() revel.Result {
+	// Expose a list view for a given model, with zeroed blacklisted field names
+	name := "oz_pools"
+	query := `
+    SELECT *
+    FROM "oz_pool"
+    `
+	result, err := Dbm.Select(models.OzPool{}, query)
+	if err != nil {
+		return c.RenderError(err)
+	}
+	out := make(map[string]interface{})
+	out[name] = result
+
+	return c.RenderJson(out)
+}
+
+/////////////////////////////////////////////////////////////////////
+
+func (c OzPools) Read(id int) revel.Result {
+	name := "oz_pools"
+	result, err := Dbm.Get(models.OzPool{}, id)
+	if err != nil {
+		return c.RenderError(err)
+	}
+	if result == nil {
+		return FResponse404(FResponse404{interface{}(map[string]string{"error": "not found"})})
+	}
+
+	out := make(map[string][]interface{})
+	out[name] = []interface{}{result}
+
+	return c.RenderJson(out)
+}
+
+/////////////////////////////////////////////////////////////////////
+
+func (c OzPools) Delete(id int) revel.Result {
+	query := `
+        DELETE
+        FROM "oz_pool"
+        WHERE id = $1
+    `
+
+	result, err := Dbm.Exec(query, id)
+	if err != nil {
+		return c.RenderError(err)
+	}
+	val, err := result.RowsAffected()
+	if err != nil {
+		return c.RenderError(err)
+	}
+	if val != 1 {
+		c.Response.Status = 500
+		return c.RenderError(errors.New("Did not delete exactly one record"))
+	}
+	return c.RenderJson(val)
+}
+
+/////////////////////////////////////////////////////////////////////
+
+func (c OzPools) Create() revel.Result {
+	return CreateList(createOzPools, c.Request.Body)
+}
+
+/////////////////////////////////////////////////////////////////////
+
+func createOzPools(data []byte) ([]interface{}, error) {
+	const keyName string = "oz_pools"
+	var typedJson map[string][]models.OzPool
+
+	err := json.Unmarshal(data, &typedJson)
+	if err != nil {
+		return nil, err
+	}
+
+	modelObjects := typedJson[keyName]
+
+	// Prepare for bulk insert (only way to do it, promise)
+	interfaces := make([]interface{}, len(modelObjects))
+	for i := range modelObjects {
+		interfaces[i] = interface{}(&modelObjects[i])
+	}
+	return interfaces, nil
+}
