a354d35f0900e74b831cdd78e520be5ed93393ae
Mike Solomon <mike@mindsnacks.com>
2013-07-06 18:59:52 -0700

    Added gorp to models

--- app/models/flesh.go
+++ app/models/flesh.go
@@ -2,8 +2,30 @@ package models
 
 import (
 	"github.com/coopernurse/gorp"
+	"github.com/robfig/revel"
+	"github.com/robfig/revel/modules/db/app"
 	"time"
 )
+
+var (
+	Dbm *gorp.DbMap
+)
+
+func init() {
+	// db.Init()
+	Dbm = &gorp.DbMap{Db: db.Db, Dialect: gorp.PostgresDialect{}}
+	Dbm.TraceOn("\x1b[36m[M.gorp]\x1b[0m", revel.INFO)
+	AddTables(Dbm)
+}
+
+func AddTables(dbm *gorp.DbMap) {
+	dbm.AddTable(Game{}).SetKeys(true, "Id")
+	dbm.AddTable(Organization{}).SetKeys(true, "Id")
+	dbm.AddTable(Player{}).SetKeys(true, "Id")
+	dbm.AddTable(User{}).SetKeys(true, "Id")
+	dbm.AddTable(Oz{}).SetKeys(true, "Id")
+	dbm.AddTableWithName(OzPool{}, "oz_pool").SetKeys(true, "Id")
+}
 
 type TimeTrackedModel struct {
 	Created *time.Time `json:"created,omitempty"`
--- app/models/player.go
+++ app/models/player.go
@@ -1,8 +1,38 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Player struct {
 	Id      int `json:"id"`
 	User_id int `json:"user_id"`
 	Game_id int `json:"game_id"`
 	TimeTrackedModel
+}
+
+func (p *Player) isZombie() bool {
+	fmt.Println(Dbm)
+	return true
+}
+
+func (p *Player) isHuman() bool {
+	return !p.isZombie()
+}
+
+func (p *Player) CanTag() (bool, error) {
+	isZ := p.isZombie()
+	if !isZ {
+		return false, errors.New("player is not a zombie!")
+	}
+	return true, nil
+}
+
+func (p *Player) CanBeTagged() (bool, error) {
+	isH := p.isHuman()
+	if !isH {
+		return false, errors.New("player is not a human!")
+	}
+	return true, nil
 }
