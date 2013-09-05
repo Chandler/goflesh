508be1188d9edda7371c26713ab23e52c5dcf1de
Mike Solomon <mike@mindsnacks.com>
2013-09-04 21:32:55 -0700

    Deploy fix

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,15 +1,17 @@
 # Assumes environment variables are set
 
+set -x
 cd $FLESH_SYNC_REPO_DIR
 git pull origin master:master
 export FLESH_COMMIT=`git rev-parse HEAD`
 mkdir -p $FLESH_ROOT_DIR/$FLESH_COMMIT
-git archive --format=tar $FLESH_COMMIT | (cd $FLESH_ROOT_DIR && tar xf -)
+git archive --format=tar $FLESH_COMMIT | (cd $FLESH_ROOT_DIR/$FLESH_COMMIT && tar xf -)
 ln -sfn $FLESH_ROOT_DIR/$FLESH_COMMIT $FLESHLOCATION
 cd $FLESHLOCATION
 cat goPackages.txt | xargs -t go get -u
 # rm $GOPATH/src/flesh
+npm install
 grunt compile
 bower install --allow-root
-cd deploy
+cd $FLESH_SYNC_REPO_DIR
 supervisorctl restart revel
